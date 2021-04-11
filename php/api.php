09d863fe6b9e24c08b3479220e0fb6c2e12c22fe
Karl Jones <karlj38@gmail.com>
2021-04-11 13:16:34 +0000

    get country list

--- php/api.php
+++ php/api.php
@@ -3,3 +3,11 @@ include "functions.php";
 
 ini_set("display_errors", "1");
 error_reporting(E_ALL);
+
+if ($api = $_GET["get"] ?? null) {
+    switch ($api) {
+        case "countryList":
+            echo getCountrylist();
+            break;
+    }
+}
--- php/functions.php
+++ php/functions.php
@@ -1,3 +1,18 @@
 <?php
 
 include "keys.php";
+
+function getCountryList()
+{
+    $json =  file_get_contents("../json/countryBorders.geo.json");
+    $data = json_decode($json)->features;
+    $countries = [];
+
+    foreach ($data as $country) {
+        $name = $country->properties->name;
+        $code = $country->properties->iso_a2;
+        array_push($countries, [$name, $code]);
+    }
+    sort($countries);
+    return json_encode($countries);
+}
