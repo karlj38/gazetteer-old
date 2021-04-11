8f4e9ba2b324696f9ffd5291cfc2cb91cbef5c44
Karl Jones <karlj38@gmail.com>
2021-04-11 12:46:09 +0000

    add tile providers

--- js/script.js
+++ js/script.js
@@ -7,7 +7,26 @@ function init() {
     ],
     zoomControl: false,
   }).fitWorld();
+
   const sat = L.tileLayer.provider("Esri.WorldImagery").addTo(map);
+  const night = L.tileLayer.provider("NASAGIBS.ViirsEarthAtNight2012");
+  const street = L.tileLayer.provider("Jawg.Streets", {
+    accessToken:
+      "Kyyk5x2h2cziidv4NudH48i2lgxN5j1e3lo5CtRHb8th7m5mbfxeq7qB71thO2ZE",
+  });
+  const dark = L.tileLayer.provider("Jawg.Dark", {
+    accessToken:
+      "Kyyk5x2h2cziidv4NudH48i2lgxN5j1e3lo5CtRHb8th7m5mbfxeq7qB71thO2ZE",
+  });
+  const baseLayers = {
+    Streets: street,
+    Dark: dark,
+    Satellite: sat,
+    Night: night,
+  };
+
+  L.control.layers(baseLayers).addTo(map);
+  L.control.zoom({ position: "bottomleft" }).addTo(map);
 }
 
 $(function () {
