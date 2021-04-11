c8e59f0a65b04dd9b7924cd568837c9040486736
Karl Jones <karlj38@gmail.com>
2021-04-11 12:48:54 +0000

    add preloader

--- js/script.js
+++ js/script.js
@@ -32,3 +32,13 @@ function init() {
 $(function () {
   init();
 });
+
+$(window).on("load", function () {
+  if ($("#preloader").length) {
+    $("#preloader")
+      .delay(100)
+      .fadeOut("slow", function () {
+        $(this).remove();
+      });
+  }
+});
